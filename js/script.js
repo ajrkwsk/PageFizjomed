bcbe419ea668ce2c59c59eda599eb33fa9d4a8bc
ajrkwsk <angelika_jurkowska@wp.pl>
2021-04-29 18:25:23 +0200

    add structure for section 'services' and add styles

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const navBar = document.querySelector('.fa-bars');
 const navCross = document.querySelector('.fa-times');
 const navDesktop = document.querySelector('.nav-desktop');
 const desktopView = window.matchMedia('screen and (min-width: 576px)');
+const card = document.querySelectorAll('.card');
 
 navBar.addEventListener('click', function() {
     navDesktop.style.display = 'block';
@@ -18,3 +19,19 @@ navCross.addEventListener('click', function(){
     navBar.style.display = 'block';
     navCross.style.display = 'none';
 })
+
+let counter = 0;
+function changeCard() {
+    for(let i = 0; i < card.length; i++) {
+        card[i].style.display = 'none';
+    }
+    counter++;
+    if(counter > card.length) {
+        counter = 1;
+    }
+    card[counter - 1].style.display = 'block';
+    setTimeout(changeCard, 3000);
+
+}
+
+// changeCard();
